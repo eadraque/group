8d9cb742be46afaacd982aca13b17bd8f642e37f
eadraque <70185780+eadraque@users.noreply.github.com>
2021-05-29 22:24:36 +0800

    Update db.js

--- server/db.js
+++ server/db.js
@@ -6,6 +6,11 @@ const pool = new Pool({
   host: "ec2-52-0-114-209.compute-1.amazonaws.com",
   port: 5432,
   database: "d32c6u3v67s1jq"
+  ssl: 
+  {
+  rejectUnauthorized : false
+}
 });
+
 
 module.exports = pool;
